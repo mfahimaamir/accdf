d33b6fd1c5221e6fade23f32a4abb4c716cbb09e
mfahimaamir <mfahim204s8@gmail.com>
2025-03-09 14:15:15 +0500

    Add files via upload

---
+++ accdf.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pyodbc
+import pandas as pd
+import numpy as np
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkinter.filedialog import askopenfile
+import os
+
+#import sqlalchemy as sa
+
+
+#cnxn   = pyodbc.connect(...)
+    
+...
+conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};' r'DBQ=C:\iqra\mfa.accdb;')
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+for row in cursor.tables():    #ok
+    print (row.table_name)    #ok
+    st.write(row.table_name)    #ok
+
+
+
+#table_name = 'DB_IMPORT_2020_PM'
+
+#engine = sa.create_engine("access+pyodbc://@my_accdb_dsn")
+#df = pd.read_sql_table(table_name, engine)
+#print(df)
+
+query = "SELECT * FROM Payments"
+dataf = pd.read_sql(query, conn)
+conn.close()
+#print(dataf)
+st.dataframe(dataf) 
+
